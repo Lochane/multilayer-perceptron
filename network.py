9cc63a06fffc40ffeab7a406908a8def9f01df84
Bugs <lochane22@gmail.com>
2026-01-04 18:34:59 +0100

    refacto as framework pytorch like

--- network.py
+++ network.py
@@ -1,4 +1,14 @@
-class SequentialNetwork:
+class Network:
+	def __init__(self):
+		self.layers = []
+
+	def forward(self, x):
+		raise NotImplementedError("Forward method not implemented.")
+
+	def backward(self, y_true, learning_rate):
+		raise NotImplementedError("Backward method not implemented.")
+
+class SequentialNetwork(Network):
 	def __init__(self, layers):
 		self.layers = layers
 
@@ -7,6 +17,9 @@ class SequentialNetwork:
 		for layer in self.layers:
 			x = layer.forward(x)
 		return x
+
+	def backward(self, y_true, learning_rate):
+		pass
 
 	def fit(self, network, data_train, data_valid, loss="None", learning_rate=0.0, batch_size=1, epochs=1):
 		pass
